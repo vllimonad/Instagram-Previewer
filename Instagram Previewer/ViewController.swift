0d6ca287a91e1f5c038e79b7e371c4ae38d994cb
Vlad Klunduk <klundukvlad40@gmail.com>
2023-10-16 22:29:34 +0200

    labels and avatar

--- Instagram Previewer/ViewController.swift
+++ Instagram Previewer/ViewController.swift
@@ -33,12 +33,20 @@ class ViewController: UIViewController {
         return button
     }()
     
-    let userAvatarButton: UIButton = {
+    var userAvatarButton: UIButton = {
         let button = UIButton(type: .system)
         button.backgroundColor = .black
         button.layer.cornerRadius = 45
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
+    }()
+    
+    let stack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.distribution = .equalSpacing
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
     }()
 
     override func viewDidLoad() {
@@ -57,14 +65,43 @@ class ViewController: UIViewController {
 
     func setupLayout() {
         view.addSubview(userAvatarButton)
+        view.addSubview(stack)
+        stack.addArrangedSubview(getLabelsWith(name: "Posts", number: 1))
+        stack.addArrangedSubview(getLabelsWith(name: "Followers", number: 245))
+        stack.addArrangedSubview(getLabelsWith(name: "Folllowing", number: 643))
+
         NSLayoutConstraint.activate([
             userAvatarButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             userAvatarButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
             userAvatarButton.widthAnchor.constraint(equalToConstant: 90),
             userAvatarButton.heightAnchor.constraint(equalToConstant: 90),
 
+            stack.leadingAnchor.constraint(equalTo: userAvatarButton.trailingAnchor, constant: 60),
+            stack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -23),
+            stack.centerYAnchor.constraint(equalTo: userAvatarButton.centerYAnchor, constant: 5)
         ])
     }
+    
+    func getLabelsWith(name: String, number: Int) -> UIStackView {
+        let nameLabel = UILabel()
+        nameLabel.text = name
+        nameLabel.font = UIFont.systemFont(ofSize: 14)
+        nameLabel.textColor = UIColor(named: "text")
+        
+        let numberLabel = UILabel()
+        numberLabel.text = String(number)
+        numberLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        numberLabel.textColor = UIColor(named: "text")
+        
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.alignment = .center
+        stack.addArrangedSubview(numberLabel)
+        stack.addArrangedSubview(nameLabel)
+        return stack
+    }
+    
+    
     
 }
 
