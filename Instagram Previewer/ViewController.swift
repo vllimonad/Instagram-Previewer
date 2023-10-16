d734935723c33fe54c1a808dd27976564affb197
Vlad Klunduk <klundukvlad40@gmail.com>
2023-10-16 19:41:22 +0200

    Initial Commit

---
+++ Instagram Previewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram Previewer
+//
+//  Created by Vlad Klunduk on 16/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
